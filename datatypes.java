b0e330ed842f5dc75036fa304def360ac685ec71
devxlalit <lalitpagare.dev@gmail.com>
2024-07-03 16:32:31 +0530

    first commit

---
+++ datatypes.java
@@ -0,0 +1,41 @@
+/**
+ * datatypes
+ */
+public class datatypes {
+
+    public static void main(String[] args) {
+        // byte a =127;
+        int a = 12;
+        float t = a;
+        System.out.println(t);
+
+
+        float b = 12.5f;
+
+        int ine = (int)b;
+        System.out.println("float to int:"+ine);
+
+
+        boolean c = false;
+        char ch = 'r';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(ch);
+        System.out.println("hello");
+
+        // ASCII values
+
+
+        int in = 43;
+        char newin = (char)in;
+        System.out.println(newin);
+
+        char cr = 'A';
+        int newcr = (int)cr;
+        System.out.println(newcr);
+
+        int $C_AR = 12;
+        System.out.println($C_AR);
+    }
+}
---
+++ operators.java
@@ -0,0 +1,27 @@
+public class operators {
+    public static void main(String[] args) {
+        // +
+        System.out.println(3/2);
+
+        int a = 12;
+        // a = a+1;
+        // a+=1;
+        a++; // increment operator
+        a--; // decrement operator
+
+        System.out.println(a++); // post increment
+        System.out.println(a); // post increment
+
+        System.out.println(++a); // pre increment
+
+
+        // Logical operator 
+        System.out.println(!(4 >= 2 && 5>7));
+        
+       
+
+
+
+
+    }
+}
